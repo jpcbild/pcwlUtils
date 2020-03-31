c45f2e20632826442d8ceeefd9fa04e977c47354
jpcbild <834147084@qq.com>
2020-03-31 18:16:59 +0800

    无赶紧要的修改直接提交

--- pcwl_mqutils/src/main/java/com/cn/pcwl/mqUtils/ActiveMqConnectionUtils.java
+++ pcwl_mqutils/src/main/java/com/cn/pcwl/mqUtils/ActiveMqConnectionUtils.java
@@ -1,13 +1,8 @@
 package com.cn.pcwl.mqUtils;
-import javax.jms.Connection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 import javax.jms.Connection;
 import javax.jms.JMSException;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class ActiveMqConnectionUtils {
 	public static Connection newConnection(String url) throws JMSException{
